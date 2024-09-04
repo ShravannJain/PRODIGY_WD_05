e1e009bb52947f8299c20b9e78e9d827fcbd32a1
shravannjain <shravanjain3065@gmail.com>
2024-09-04 19:35:05 +0530

    connected to weatherapp api to fetch the data

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const apiKey = "baffd41c6993ab31856e392d26ce4cef";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany";
 
+async function checkWeather() {
+ const response=await fetch(apiUrl +`&appid=${apiKey}`);
+ var data=await response.json();
+    console.log(data);
+    document.querySelector(".weather-place").innerHTML=data.name;
+    document.querySelector(".temparature").innerHTML=data.name;
+    document.querySelector(".weather-place").innerHTML=data.name;
+}
+
+checkWeather();
