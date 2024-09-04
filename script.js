e3aa2ff42ebb8e642ad09819e904f06340414aee
shravannjain <shravanjain3065@gmail.com>
2024-09-04 22:27:16 +0530

    displayed the previously fetched data through api

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const apiKey = "baffd41c6993ab31856e392d26ce4cef";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=germany";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=mangalore";
 
 async function checkWeather() {
  const response=await fetch(apiUrl +`&appid=${apiKey}`);
  var data=await response.json();
     console.log(data);
     document.querySelector(".weather-place").innerHTML=data.name;
-    document.querySelector(".temparature").innerHTML=data.name;
+    document.querySelector(".temperature").innerHTML=data.main.temp;
     document.querySelector(".weather-place").innerHTML=data.name;
+    document.querySelector("#humidity").innerHTML=data.main.humidity;
+    document.querySelector("#Visibility").innerHTML=data.visibility;
+    document.querySelector("#wind").innerHTML=data.wind.speed;
 }
 
 checkWeather();
